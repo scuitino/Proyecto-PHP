c791f11fd0f7be981a2a71a2b54e8ead47f03c44
Alejandro <aale21@gmail.com>
2014-07-12 20:09:36 -0300

    Agrego la relación del modelo Imagen a Inmueble (Supongo que se habría quitado por error)

--- Aplicacion/protected/models/Imagen.php
+++ Aplicacion/protected/models/Imagen.php
@@ -44,6 +44,7 @@ class Imagen extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
+			'inmuebleIdInmueble' => array(self::BELONGS_TO, 'Inmueble', 'Inmueble_idInmueble'),
 		);
 	}
 
